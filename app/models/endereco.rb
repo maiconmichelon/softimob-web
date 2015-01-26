20d823fe3c7816361ef2c3f1e31a03ed5b3f12aa
Maicon Michelon <maicon.fermo@gmail.com>
2015-01-26 15:19:41 +0000

    Mexendo no form de endereço

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -4,6 +4,18 @@ class Endereco < ActiveRecord::Base
   validates :cep, :rua, presence: true
   validates :cep, length: { is: 8 }
 
+  def estado_id
+    self.rua.nil? ? nil : self.rua.bairro.municipio.estado.id
+  end
+  
+  def municipio_id
+    self.rua.nil? ? nil : self.rua.bairro.municipio.id
+  end
+  
+  def bairro_id
+    self.rua.nil? ? nil : self.rua.bairro.id
+  end
+  
   def to_s
     "#{rua}, #{rua.bairro.municipio}-#{rua.bairro.municipio.estado.uf}"
   end
